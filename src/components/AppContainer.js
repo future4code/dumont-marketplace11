bf864227739a0c2877141d4d1a7c5fa901061f22
laispetra <63520023+laispetra@users.noreply.github.com>
2020-11-03 11:58:01 -0300

    Initial commit

---
+++ src/components/AppContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class AppContainer extends Component {
+  render() {
+    return (
+      <div>
+        <p>Pronto para começar!</p>
+      </div>
+    )
+  }
+}
