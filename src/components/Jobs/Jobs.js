2296d01c94fbe39a2a908a86e68c63b82a740302
b-bianca <you@example.com>
2020-11-05 15:47:36 -0300

    finalmente detalhes do card aparecendo

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 import JobsGridCard from './JobsGridCard'
 import axios from 'axios'
-
 
 const urlBase = "https://us-central1-labenu-apis.cloudfunctions.net/futureNinjasOne/jobs"
 
 
- class Jobs extends React.Component {
+class Jobs extends React.Component {
     state = {
-        allCards: []
+        allCards: [],
+        seeInfo: false,
+        cardDet: ""
     }
 
     componentDidMount() {
@@ -25,15 +26,14 @@ const urlBase = "https://us-central1-labenu-apis.cloudfunctions.net/futureNinjas
     }
 
 
-
-  render() {
-    return (
+   render() {
+        return (
       <div>
-        <JobsGridCard
-      allCards = {this.state.allCards}
-      /> 
+         <JobsGridCard
+          allCards = {this.state.allCards}
+          />
       </div>
-              
+
     )
   }
 }
--- src/components/Jobs/JobsGridCard.js
+++ src/components/Jobs/JobsGridCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import JobCard from './JobCard'
+import JobCardDetails from './JobCardDetails'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -10,17 +11,45 @@ const Container = styled.div`
 `
 
 class JobsGridCard extends React.Component {
+    state = {
+        seeInfo: false,
+        idCard: ""
+    }
 
-	render(){
+    seeDetails = (id) => {
+        console.log("detalhess")
+        this.setState({
+          seeInfo: true,
+          idCard: id
+        })
+    }
+
+   closeCard = () => {
+       this.setState({seeInfo: false})
+   }
+
+    render(){
+       
+       const renderSeeDet = this.state.seeInfo ? <JobCardDetails idProps = {this.state.idCard} close= {this.closeCard} /> : null
+
+
         const renderedJobs = this.props.allCards.map((item) => {
-            return (<JobCard key={item.id} title={item.title} 
-                value={item.value} dueDate={item.dueDate}/>)
+            return (<JobCard key={item.id} id={item.id} title={item.title} 
+                value={item.value} dueDate={item.dueDate}  detailsTeste={this.seeDetails}/>)
         })
 
 		return (
+            
 			<Container>
                 {renderedJobs}
+                {renderSeeDet}
             </Container>
+
+            
+                
+            
+
+            
 		)
 	}
 }
